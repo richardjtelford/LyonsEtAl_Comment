f301b765ba9d56473f0d386ff95ee90caf19f8be
richardjtelford <richard.telford@bio.uib.no>
2016-05-31 07:56:01 +0200

    using new data. removing weighs and log scale

--- ED_fig2.R
+++ ED_fig2.R
@@ -1,29 +1,36 @@
 library("ggplot2")
 library("scales")
+library("readxl")
+library("dplyr")
+
 
 #load data
-combETE <- read.csv("./data/combinedETE.csv")
-combETE$old <- combETE$Age > 100
+comb <- read_excel("data/comb.data_revised_for_Telford.xlsx", sheet = 1)
 
-#with weights = numSig taxa
+comb$AGE <- cut(comb$age.published, breaks = c(0, 100, 1000000, 10^9), labels = c("Modern", "Shallow Time", "Deep Time"))
 
-ggplot(subset(combETE, land.island != "island"), aes(x = numSites, y = percAgg, col = old, shape = old, weight = numSig)) + 
-  geom_jitter(width = 0, height = 0.02) +
-  geom_smooth(aes(group  = 1), method = "gam", formula = y ~s(x, k = 4), method.args = list(family = "quasibinomial"), show.legend = FALSE) +
-  scale_y_continuous(labels = percent) +
-  scale_x_log10(breaks = c(10, 20, 50, 100, 200, 400)) + 
-  scale_colour_discrete(breaks = c("TRUE","FALSE"), labels = c("True", "False")) +
-  scale_shape_discrete(breaks = c("TRUE","FALSE"), labels = c("True", "False")) +
-  labs(x = "Number of sites", y = "Percent aggregated pairs", colour = "Age > 100", shape = "Age > 100") + 
-  theme(legend.justification=c(0, 0), legend.position=c(.76, 0)) 
-ggsave("ed_fig2.png", height = 89 * 1.5, width = 89 * 1.5, units = "mm")
+comb2 <- comb %>%
+  filter(!land.island.published %in% c("island", "NA"))
+
+comb2 %>%
+  group_by(AGE) %>%
+  summarise(n = n(), lt40 = mean(numSites < 40))
+
+table(comb2$AGE)
 
 #no weights
-ggplot(subset(combETE, land.island != "island"), aes(x = numSites, y = percAgg, col = land.island, shape = land.island)) + 
-  geom_jitter(width = 0, height = 0.03) +
+g <- ggplot(comb2, aes(x = numSites, y = percAgg, col = AGE, shape = AGE)) + 
+  geom_jitter(width = 0, height = 0.02) +
   geom_smooth(aes(group  = 1), method = "gam", formula = y ~s(x), method.args = list(family = "quasibinomial"), show.legend = FALSE) +
-  scale_y_continuous(labels = percent) +
-  scale_x_log10() + 
-  labs(x = "Number of sites", y = "Percent aggregated pairs", colour = "Age", shape = "Age")
-       
+  labs(x = "Number of sites", y = "Proportion aggregated pairs", colour = "Age", shape = "Age") + 
+  theme(legend.justification=c(0, 0), legend.position=c(.76, 0)) 
+print(g)
+ggsave("ed_fig2.png", height = 89 * 1.5, width = 89 * 1.5, units = "mm")
+
+g + geom_vline(xintercept = c(20, 30, 40, 50))
+
+g <- ggplot(subset(combETE, land.island != "island"), aes(x = Age, y =numSites)) + 
+  geom_jitter(width = 1, height = 0.02)+
+  scale_x_log10() + geom_vline(xintercept = 6000)
+print(g)
 
